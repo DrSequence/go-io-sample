ca25673e699e0473321fffea0e04804cebef2fe8
ilya <ilya.selfrealization@gmail.com>
2021-10-29 10:56:23 +0300

    temp stage

--- cmd/v2/main.go
+++ cmd/v2/main.go
@@ -6,21 +6,26 @@ import (
 	"io"
 	"log"
 	"os"
-	"unicode"
+	"time"
 )
 
+var isHeaderRead = false
+var position = 1
+
 func main() {
-	inFile, _ := os.Open("example.csv")
+	start := time.Now()
+	log.Println("starting read/write...")
+	inFile, _ := os.Open("csv/example.csv")
 	defer inFile.Close()
 
-	outFile, _ := os.OpenFile("example.csv", os.O_RDWR, 0777)
+	outFile, _ := os.OpenFile("csv/example.csv", os.O_RDWR, 0777)
 	defer outFile.Close()
 
 	reader := bufio.NewReaderSize(inFile, 10*1024)
 
 	for {
 		line, err := reader.ReadString('\n')
-		log.Println("line:", line)
+		log.Println("|readliner|line|:", line)
 		outFile.WriteString(changeData(line))
 		if err != nil {
 			if err != io.EOF {
@@ -29,22 +34,41 @@ func main() {
 			break
 		}
 	}
+
+	log.Println("done. Time elapsed: ", time.Since(start))
 }
 
 func changeData(data string) string {
-	log.Println("data:", data)
+	// find fist and second ;
+	// save positions: global - it's nessesary
+	// find get substring
+	// create hex
+	// overwrite substring
+	// return
 
-	o := make([]rune, 0, len(data))
-	for _, r := range data {
-		if unicode.IsLetter(r) {
-			if unicode.IsUpper(r) {
-				o = append(o, unicode.ToLower(r))
-			} else {
-				o = append(o, unicode.ToUpper(r))
-			}
-		} else {
-			o = append(o, r)
+	if !isHeaderRead {
+		isHeaderRead = true
+		return data
+	} else {
+		// o := make([]rune, 0, len(data))
+		for _, record := range data {
+			log.Println("record:", record)
 		}
+
+		return "\n"
 	}
-	return string(o)
+
+	// o := make([]rune, 0, len(data))
+	// for _, r := range data {
+	// 	if unicode.IsLetter(r) {
+	// 		if unicode.IsUpper(r) {
+	// 			o = append(o, unicode.ToLower(r))
+	// 		} else {
+	// 			o = append(o, unicode.ToUpper(r))
+	// 		}
+	// 	} else {
+	// 		o = append(o, r)
+	// 	}
+	// }
+
 }
